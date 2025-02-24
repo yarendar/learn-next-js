0d1c258ba58f5aee589e6e41a5794c1b95b11016
yarendar <yuriy.arendar@gmail.com>
2025-02-24 15:24:05 +0200

    setup tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-white">
+    <h1 className="text-3xl text-primary-500">
       Welcome to the world of next.js
     </h1>
   );
