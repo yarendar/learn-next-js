7acd8e8fe641068796d93d1dd0885204e121861c
yarendar <yuriy.arendar@gmail.com>
2025-02-27 11:39:43 +0200

    created theme switcher component

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Theme from "@/components/navigation/navbar/Theme";
 
 const Navbar = () => {
   return (
@@ -20,7 +21,9 @@ const Navbar = () => {
 
       <p>Global search</p>
 
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
