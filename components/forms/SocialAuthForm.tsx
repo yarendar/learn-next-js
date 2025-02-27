bc21153d70fa125f0e33dc3e82abe55ea05daa00
yarendar <yuriy.arendar@gmail.com>
2025-02-27 17:22:55 +0200

    social auth form

---
+++ components/forms/SocialAuthForm.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const SocialAuthForm = () => {
+  const buttonClass =
+    "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+
+  return (
+    <div className="mt-10 flex flex-wrap gap-2.5">
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/github.svg"
+          alt="Gihub Logo"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+        <span>Log in with GitHub</span>
+      </Button>
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/google.svg"
+          alt="Google Logo"
+          width={20}
+          height={20}
+          className="mr-2.5 object-contain"
+        />
+        <span>Log in with Google</span>
+      </Button>
+    </div>
+  );
+};
+
+export default SocialAuthForm;
