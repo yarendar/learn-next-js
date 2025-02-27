dd92f5c93274dad60454c90d6a9d1449282be626
yarendar <yuriy.arendar@gmail.com>
2025-02-27 11:11:13 +0200

    fixed hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressContentEditableWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
