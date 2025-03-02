8289ef9e70151c40dd55479bb69df8b0c02b7f5b
yarendar <yuriy.arendar@gmail.com>
2025-03-02 16:14:53 +0200

    filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
 import LocalSearch from "@/components/search/LocalSearch";
+import HomeFilter from "@/components/filters/HomeFilter";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -66,7 +67,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           otherClasses="flex-1"
         />
       </section>
-      {/*HomeFilter*/}
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
---
+++ components/filters/HomeFilter.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
+
+const filters = [
+  { name: "React", value: "react" },
+  { name: "JavaScript", value: "javascript" },
+  // { name: "Newest", value: "newest" },
+  // { name: "Popular", value: "popular" },
+  // { name: "Unanswered", value: "unanswered" },
+  // { name: "Recommended", value: "recommended" },
+];
+
+const HomeFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (filter: string) => {
+    let newUrl;
+
+    if (filter === active) {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["filter"],
+      });
+
+      setActive("");
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: filter.toLowerCase(),
+      });
+
+      setActive(filter);
+    }
+
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <div className="mt-10 flex-wrap gap-3 md:flex">
+      {filters.map((filter) => (
+        <Button
+          key={filter.name}
+          className={cn(
+            `body-medium rounded-lg px-6 py-3 capitalize shadow-none`,
+            active === filter.value
+              ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300",
+          )}
+          onClick={() => handleTypeClick(filter.value)}
+        >
+          {filter.name}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default HomeFilter;
