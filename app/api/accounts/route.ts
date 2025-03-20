8a45426d06c1bd91a6627a31918a45d7d2e4149b
yarendar <yuriy.arendar@gmail.com>
2025-03-20 16:16:45 +0200

    implement create and read account routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,50 @@
+import handleError from "@/lib/handlers/error";
+import { APIErrorResponse } from "@/types/global";
+import dbConnect from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+import { AccountSchema } from "@/lib/validations";
+import { ForbiddenError } from "@/lib/http-errors";
+import Account from "@/database/account.model";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const accounts = await Account.find();
+
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 },
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+
+    const validatedData = AccountSchema.safeParse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+
+    if (existingAccount) {
+      throw new ForbiddenError(`Account with the same provider already exists`);
+    }
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 },
+    );
+  } catch (e) {
+    return handleError(e, "api") as APIErrorResponse;
+  }
+}
