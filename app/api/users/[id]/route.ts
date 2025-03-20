663f7b9c3c2bfdd14ee0ed18d3194df51314adf0
yarendar <yuriy.arendar@gmail.com>
2025-03-20 09:04:04 +0200

    user routes

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,77 @@
+import handleError from "@/lib/handlers/error";
+import { APIErrorResponse } from "@/types/global";
+import { NotFoundError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import User from "@/database/user.model";
+import { NextResponse } from "next/server";
+import { UserSchema } from "@/lib/validations";
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+    const user = await User.findById(id);
+
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 204 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData = UserSchema.partial().parse(body);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      { success: true, data: updatedUser },
+      { status: 204 },
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
