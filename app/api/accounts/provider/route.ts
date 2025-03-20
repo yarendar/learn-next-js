24d72b61f71bbcb13e73004ad17393e1fa2905f4
yarendar <yuriy.arendar@gmail.com>
2025-03-20 16:28:43 +0200

    implement account by provider routes

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,30 @@
+import handleError from "@/lib/handlers/error";
+import { APIErrorResponse } from "@/types/global";
+import { AccountSchema } from "@/lib/validations";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+import Account from "@/database/account.model";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    await dbConnect();
+
+    const account = await Account.findOne({ providerAccountId });
+
+    if (!account) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
