7ae9f907283bb2419822602fe40662a2aca1fbc2
yarendar <yuriy.arendar@gmail.com>
2025-04-01 17:41:42 +0300

    implement get all users action

--- types/global.d.ts
+++ types/global.d.ts
@@ -67,3 +67,15 @@ interface Collection {
   author: string | Author;
   question: Question;
 }
+
+interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
