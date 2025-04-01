10d2c331d0960a143c73582569bf1e7b5d86df13
yarendar <yuriy.arendar@gmail.com>
2025-04-01 18:11:50 +0300

    implement display all users

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { getUsers } from "@/lib/actions/user.action";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_USERS } from "@/constants/states";
+import UserCard from "@/components/cards/UserCard";
 
-const Community = () => {
-  return <div>community</div>;
+const Community = async ({ searchParams }: RouteParams) => {
+  const { page, pageSize, query, filter } = await searchParams;
+
+  const { success, data, error } = await getUsers({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { users } = data || {};
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">All users</h1>
+
+      <div className="mt-11">
+        <LocalSearch
+          route={ROUTES.COMMUNITY}
+          imgSource={"/icons/search.svg"}
+          placeholder="Search some great devs..."
+          iconPosition={"left"}
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <DataRenderer
+        success={success}
+        data={users}
+        empty={EMPTY_USERS}
+        error={error}
+        render={(users) => (
+          <div className="mt-12 flex flex-wrap gap-5">
+            {users.map((user) => (
+              <UserCard key={user._id} {...user} />
+            ))}
+          </div>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Community;
--- constants/states.ts
+++ constants/states.ts
@@ -52,3 +52,9 @@ export const EMPTY_COLLECTIONS = {
     href: ROUTES.COLLECTION,
   },
 };
+
+export const EMPTY_USERS = {
+  title: "No Users Found",
+  message:
+    "It's ALONE in the world. You're the only one here. Join us and let the world know who you are.",
+};
