d9ac28c12fa5add0275f961e9f899ad2d9a4fa87
yarendar <yuriy.arendar@gmail.com>
2025-04-01 09:53:35 +0300

    fix vote issue

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -72,7 +72,9 @@ export async function createVote(
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) {
+    return handleError(new Error("Unauthorized")) as ErrorResponse;
+  }
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -98,6 +100,10 @@ export async function createVote(
           existingVote._id,
           { voteType },
           { new: true, session },
+        );
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session,
         );
         await updateVoteCount(
           { targetId, targetType, voteType, change: 1 },
