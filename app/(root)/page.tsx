829a86f9f449dff79de3a2c539c44c16b9bb212a
yarendar <yuriy.arendar@gmail.com>
2025-04-02 09:38:19 +0300

    implement filters component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
 import Link from "next/link";
-import LocalSearch from "@/components/search/LocalSearch";
-import HomeFilter from "@/components/filters/HomeFilter";
+
 import QuestionCard from "@/components/cards/QuestionCard";
-import { getQuestions } from "@/lib/actions/queston.action";
 import DataRenderer from "@/components/DataRenderer";
+import CommonFilter from "@/components/filters/CommonFilter";
+import HomeFilter from "@/components/filters/HomeFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
+import { getQuestions } from "@/lib/actions/queston.action";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -22,12 +25,12 @@ const Home = async ({ searchParams }: SearchParams) => {
     filter: filter || "",
   });
 
-  const { questions } = data || [];
+  const { questions } = data || {};
 
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All questions</h1>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
         <Button
           className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
@@ -36,19 +39,26 @@ const Home = async ({ searchParams }: SearchParams) => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-      <section className="mt-11">
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           route="/"
           imgSource="/icons/search.svg"
-          placeholder="Search Questions..."
+          placeholder="Search questions..."
           otherClasses="flex-1"
+        />
+
+        <CommonFilter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
         />
       </section>
       <HomeFilter />
+
       <DataRenderer
         success={success}
-        data={questions}
         error={error}
+        data={questions}
         empty={EMPTY_QUESTION}
         render={(questions) => (
           <div className="mt-10 flex w-full flex-col gap-6">
