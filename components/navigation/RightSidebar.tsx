f5208c4539c14796ea085817fffc9658e9d2c0bc
yarendar <yuriy.arendar@gmail.com>
2025-04-03 08:59:53 +0300

    show top questions and tags

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -8,12 +8,10 @@ import { getHotQuestions } from "@/lib/actions/queston.action";
 import { getTopTags } from "@/lib/actions/tag.action";
 
 const RightSidebar = async () => {
-  const { success, data: hotQuestions, error } = await getHotQuestions();
-  const {
-    success: tagSuccess,
-    data: tags,
-    error: tagError,
-  } = await getTopTags();
+  const [
+    { success, data: hotQuestions, error },
+    { success: tagSuccess, data: tags, error: tagError },
+  ] = await Promise.all([getHotQuestions(), getTopTags()]);
 
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
